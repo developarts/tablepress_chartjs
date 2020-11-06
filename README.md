c8701c23997335aa0fc04c4509818ea25bb88d48
Developarts <nexus@developarts.com>
2020-11-06 16:34:27 -0600

    v0.2
    ### [0.2] - 2020-11-06
    
    #### Added
    - Attribute `height` for the canvas object
    - Attribute `color={color1,color2,..n}` comma separator
    - Supported colors [blue,red,orange,yellow,green,purple,grey,black]
    - Attribute `first={n}` show only `{n}` first rows
    - Attribute `last={n}` show only `{n}` last rows
    
    #### Changed
    - Attribute `data` now accept A-Z to correspond TablePress columns
    - Attribute `dimension` to `label` attribute
    - `_maybe_string_to_number` RegExp

--- README.md
+++ README.md
@@ -1,31 +1,158 @@
 # TablePress Extension: Chart.js
-Create a chart.js using the data in a TablePress table
+
+Create a [Chart.js](https://www.chartjs.org/) using [TablePress](https://tablepress.org/) as data source
 
 
 ## About
 
-This plugin code and idea is a modification of [TablePress Extension: Chartist](https://github.com/soderlind/tablepress_chartist).
-
-Using [Chart.js](https://www.chartjs.org/), this [TablePress](https://tablepress.org/) Extension creates a responsive chart based on the data in a TablePress table.
+This plugin code base and idea is a modification of [TablePress Extension: Chartist](https://github.com/soderlind/tablepress_chartist).
 
 
 ## Usage
 
-Add the Shortcode [tp-chartjs id=123 dimension=1 data=2,3/] to a post or page to create a chart from the TablePress table 123.
+Add the Shortcode `[tp-chartjs id=1 label=A data=B,C/]` to a post or page to create a chart from the TablePress ID '1' where labes are column `A`, and dataset points are columns `B` and `C`.
 
+![TablePress Usage](assets/tp_usage.png)
+
+Result:
+
+![Chartjs Usage](assets/chart_usage.png)
+
+
+## Attributes
+
+ - [`id={n}`](#att_id) TablePress ID reference
+ - [`label={c,...}`](#att_label) Column Axis label (default: A)
+ - [`data={n,...}`](#att_data) Column(s) datasets used to populate chart (default: B)
+ - [`chart=[line|hbar]`](#att_chart) Chart type (default: line)
+ - [`color`](#att_color) (default: blue,red,orange,yellow,green,purple,grey,black)
+ - [`height={n}`](#att_height) Declare height of `canvas` HTML object
+ - [`first={n}`](#att_first) Declare that only use the first `{n}` rows of data
+ - [`last={n}`](#att_last) Declare that only use the last `{n}` rows of data
+
+
+### `id`<a id="att_id"></a>
+
+TablePress ID reference
+
+* **Example:** `[tp-chartjs id=1/]`
+* **Value:** Integer `id`
+
+
+### `label`<a id="att_label"></a>
+
+Column Axis label
+
+* **Example:** `[tp-chartjs id=1 label=A data=B,C/]`
+* **Value:** One character from [A-Z]
+* **Default:** `A`
+
+
+### `data`<a id="att_data"></a>
+
+Column(s) datasets used to populate chart
+
+* **Example:** `[tp-chartjs id=1 label=A data=B,C,D,E/]`
+* **Values:** One or more comma separated columns character [A-Z]
+* **Default:** `B`
+
+
+### `chart`<a id="att_chart"></a>
+
+Chart type used
+
+* **Example:** `[tp-chartjs id=1 label=A data=B,C chart=line/]`
+* **Values:** Select one of this
+    * `line`: Line
+    * `hbar`: Vertical Bar
+* **Default:** `line`
+
+
+### `color`<a id="att_color"></a>
+
+The colors you can use in populated lines or bars.
+
+* **Example:** `[tp-chartjs id=1 label=A data=B,C color=blue,red/]`
+* **Values:** One or more comma separated colors
+    * `blue`
+    * `red`
+    * `orange`
+    * `yellow`
+    * `green`
+    * `purple`
+    * `grey`
+    * `black`
+* **Default:** `blue,red,orange,yellow,green,purple,grey,black`
+
+On example column `B` draws `blue` and column `C` draws `red`
+
+
+### `height`<a id="att_height"></a>
+
+Declare height of `canvas` HTML object
+
+* **Example:** `[tp-chartjs id=1 label=A data=B,C height=300/]`
+* **Values:** Integer value transformed into pixels
+* **Default:** Automatic
+
+### `first`<a id="att_first"></a>
+
+Declare that only use the first `{n}` rows of data
+
+* **Example:** `[tp-chartjs id=1 label=A data=B,C first=10/]`
+* **Value:** Integer value offset
+* **Default:** All data
+
+
+### `last`<a id="att_last"></a>
+
+Declare that only use the last `{n}` rows of data
+
+* **Example:** `[tp-chartjs id=1 label=A data=B,C last=10/]`
+* **Values:** Integer value offset
+* **Default:** All data
 
 
 ## Install
 
+Prerequisite: The [TablePress](https://tablepress.org/) plugin
 
-## Donations
 
-[Buy me a Coffee](https://www.paypal.com/paypalme/developarts)
+## Donate to Developer
+
+If you like my work, please donate to help me.
+
+[![Donate to Developer](assets/button-donate.png)](https://www.paypal.com/donate?hosted_button_id=ZXY9DM6PTWB8C)
+
+
+## To Do
+
+- [ ] Chart Title
+- [ ] Vertical Bar chart support
+- [ ] Pie chart support
+- [ ] Time Series chart support
+- [ ] Stacked chart support
 
 
 ## Changelog
 
-* 0.1 Initial release
+### [0.2] - 2020-11-06
+
+#### Added
+- Attribute `height` for the canvas object
+- Attribute `color={color1,color2,..n}` comma separator
+- Supported colors [blue,red,orange,yellow,green,purple,grey,black]
+- Attribute `first={n}` show only `{n}` first rows
+- Attribute `last={n}` show only `{n}` last rows
+
+#### Changed
+- Attribute `data` now accept A-Z to correspond TablePress columns
+- Attribute `dimension` to `label` attribute
+- `_maybe_string_to_number` RegExp
+
+
+### [0.1] - 2020-11-01
+- Initial release
 
 
 ## Credits
--- tablepress_chartjs.php
+++ tablepress_chartjs.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: TablePress Extension: Chart.js
 Plugin URI: https://github.com/developarts/tablepress_chartjs
-Description: Extension for TablePress to create a responsive chart based on the data in a TablePress table.
-Version: 0.1
+Description: Extension for TablePress to create a responsive Chart.js based on the data in a TablePress table.
+Version: 0.2
 Author: Alejandro García
 Author URI: https://developarts.com
 License: GPL
@@ -18,106 +18,91 @@ defined('ABSPATH') || die('No direct script access allowed!');
 add_action('tablepress_run', ['TablePress_Chartjs', 'init']);
 
 /**
- * Class that contains the TablePress chartjs Extension functionality.
+ * Class that contains the TablePress Chart.js Extension functionality.
  *
  * @author Per Soderlind, Tobias Bäthge
- *
- * @since 0.1
+ * @author DevelopArts, Alejandro García
  */
 class TablePress_Chartjs
 {
     /**
      * Version number of the Extension.
-     *
-     * @since 0.1
-     *
      * @var string
      */
-    protected static $version = '0.1';
+    protected static $version = '0.2';
 
     /**
      * Available Shortcode attributes, without the `chartjs_` prefix.
-     *
-     * @since 0.2
-     *
      * @var array
      */
     protected static $shortcode_attributes = [
-        'low'              => null,
-        'high'             => null,
-        'width'            => '',
         'height'           => '',
+        'width'            => '',
         'chart'            => 'line',
-        'showline'         => true,
-        'showarea'         => false,
-        'showpoint'        => true,
-        'linesmooth'       => true,
-        'aspect_ratio'     => '3:4',
-        'horizontal'       => false,
-        'stack'            => false,
-        'animation'        => false,
-        'label_offset'     => false,
-        'chart_padding'    => false,
-        'donut_width'      => false,
-        'label'            => 1,
-        'data'             => ''
+        'color'            => 'blue,red,orange,yellow,green,purple,grey,black',
+        'label'            => 'a',
+        'data'             => 'b',
+        'last'             => '',
+        'first'            => '',
     ];
 
     /**
-     * Mapping of some Shortcode parameters to their chartjsJS equivalent.
-     *
-     * @since 0.2
-     *
+     * Mapping of columns char
      * @var array
      */
-    protected static $attribute_to_js_mapping = [
-        'low'              => 'low',
-        'high'             => 'high',
-        'showline'         => 'showLine',
-        'showarea'         => 'showArea',
-        'showpoint'        => 'showPoint',
-        'linesmooth'       => 'lineSmooth',
-        'horizontal'       => 'horizontalBars',
-        'stack'            => 'stackBars',
-        'label_offset'     => 'labelOffset',
-        'chart_padding'    => 'chartPadding',
-        'donut_width'      => 'donutWidth',
-        'label'            => 'label',
-        'data'             => 'data'
-    ];
+    protected static $columns = array(
+        'a',        'b',        'c',        'd',        'e',
+        'f',        'g',        'h',        'i',        'j',
+        'k',        'l',        'm',        'n',        'o',
+        'p',        'q',        'r',        's',        't',
+        'u',        'v',        'w',        'x',        'y',
+        'z'
+    );
+
+    /**
+     * Mapping colors
+     * @var array
+     */
+    protected static $colors = array(
+        'blue'      => 'rgb(54, 162, 235)',
+        'red'       => 'rgb(255, 99, 132)',
+        'orange'    => 'rgb(255, 159, 64)',
+        'yellow'    => 'rgb(255, 205, 86)',
+        'green'     => 'rgb(75, 192, 192)',
+        'purple'    => 'rgb(153, 102, 255)',
+        'grey'      => 'rgb(201, 203, 207)',
+        'black'     => 'rgb(0, 0, 0)',
+    );
 
 
     /**
-     * Register necessary plugin filter hooks and the [table-chart] Shortcode.
-     *
-     * @since 0.1
+     * Register necessary plugin filter hooks and the [tp-chartjs] Shortcode.
      */
-    public static function init()
+    public static function init ()
     {
         add_action('wp_enqueue_scripts', [__CLASS__, 'enqueue_scripts_styles']);
         add_filter('tablepress_shortcode_table_default_shortcode_atts', [__CLASS__, 'register_shortcode_attributes']);
         add_filter('tablepress_table_output', [__CLASS__, 'generate_chart'], 10, 3);
-        add_shortcode('table-chart', [__CLASS__, 'handle_table_chart_shortcode']);
+        add_shortcode('tp-chartjs', [__CLASS__, 'handle_table_chart_shortcode']);
     }
 
+
     /**
-     * Handle Shortcode [table-chart id=<ID> /] in `the_content()`.
-     *
-     * @since 0.6
+     * Handle Shortcode [tp-chartjs id=<ID> /] in `the_content()`.
      *
      * @param array $shortcode_atts List of attributes that where included in the Shortcode.
-     *
      * @return string Generated HTML code for the chart with the ID <ID>.
      */
-    public static function handle_table_chart_shortcode($shortcode_atts)
+    public static function handle_table_chart_shortcode ($shortcode_atts)
     {
         // Generate the attribute query array for the template tag function.
-        //print_r($shortcode_atts);
         $table_query = [
             'chartjs' => true,
         ];
+
         // Pass all parameters to the template tag parameters.
         foreach ((array) $shortcode_atts as $attribute => $value) {
+
             // Prepend 'chartjs_' to all Shortcode attributes that the Extension understands.
             if (isset(self::$shortcode_attributes[$attribute])) {
                 $attribute = 'chartjs_'.$attribute;
@@ -125,19 +110,15 @@ class TablePress_Chartjs
             $table_query[$attribute] = $value;
         }
 
-        //print_r($attribute);
-
     	if (function_exists('tablepress_get_table')) {
             	return tablepress_get_table($table_query);
     	}
     }
 
     /**
-     * Load chartjs JavaScript and CSS files.
-     *
-     * @since 0.1
+     * Load Chart.js JavaScript and CSS files.
      */
-    public static function enqueue_scripts_styles()
+    public static function enqueue_scripts_styles ()
     {
         global $post;
         if (is_a($post, 'WP_Post') && has_shortcode($post->post_content, 'tp-chartjs')) {
@@ -146,18 +127,15 @@ class TablePress_Chartjs
         }
     }
 
+
     /**
      * Add the Extension's parameters as valid [table /] Shortcode attributes.
      *
-     * @since 0.1
-     *
      * @param array $default_atts Default attributes for the TablePress [table /] Shortcode.
-     *
      * @return array Extended attributes for the Shortcode.
      */
-    public static function register_shortcode_attributes($default_atts)
+    public static function register_shortcode_attributes ($default_atts)
     {
-        //print_r($default_atts);
         $default_atts['chartjs'] = false;
         foreach (self::$shortcode_attributes as $attribute => $value) {
             $default_atts['chartjs_'.$attribute] = $value;
@@ -165,10 +143,9 @@ class TablePress_Chartjs
         return $default_atts;
     }
 
+
     /**
      * Generate the HTML and JavaScript code for a chartjs chart, based on the data of the given table.
-     *
-     * @since 0.1
      *
      * @param string $output         The generated HTML for the table.
      * @param array  $table          The current table.
@@ -176,7 +153,7 @@ class TablePress_Chartjs
      *
      * @return string The generated HTML and JavaScript code for the chart.
      */
-    public static function generate_chart($output, $table, $render_options)
+    public static function generate_chart ($output, $table, $render_options)
     {
 
         if (!$render_options['chartjs']) {
@@ -196,83 +173,114 @@ class TablePress_Chartjs
         }
 
 
-        // Colors
-        $colors = array(
-            'rgb(54, 162, 235)',     // blue
-            'rgb(255, 99, 132)',     // red
-			'rgb(255, 159, 64)',     // orange
-			'rgb(255, 205, 86)',     // yellow
-			'rgb(75, 192, 192)',     // green
-			'rgb(153, 102, 255)',    // purple
-			'rgb(201, 203, 207)',    // grey
-        );
-
         // Declare
         $datasets = [];
 
-        // Labels
-        $datasets['label']['data'] = array_column($table['data'], $render_options['chartjs_label'] - 1);
-        $datasets['label']['name'] = $datasets['label']['data'][0];
-        array_shift($datasets['label']['data']);
-        $datasets['label']['json'] = json_encode($datasets['label']['data']);
+        // Read all data columns
+        foreach ($table['data'][0] as $key => $column) {
+            $letter = self::$columns[$key]; // column letter
+            $tempdata = [];
 
-        // Data Sets
-        $sdata = explode(',', $render_options['chartjs_data']);
-        foreach ($sdata as $key => $value) {
-            $datasets['sets'][$key]['data'] = array_column($table['data'], $value - 1);
-            $datasets['sets'][$key]['name'] = $datasets['sets'][$key]['data'][0];
-            array_shift($datasets['sets'][$key]['data']);
-            foreach ($datasets['sets'][$key]['data'] as $ckey => $cvalue) {
-                $datasets['sets'][$key]['data'][$ckey] = self::_maybe_string_to_number($cvalue);
+            $tempdata = array_column($table['data'], $key); // all data in array
+            array_shift($tempdata); // Delete first row (name of column)
+
+            // Check firts or last
+            if (is_int((int)$render_options['chartjs_last'])) {
+                $tempdata = array_slice($tempdata, (int)$render_options['chartjs_last'] * -1);
+            } elseif (is_int((int)$render_options['chartjs_first'])) {
+                $tempdata = array_slice($tempdata, (int)$render_options['chartjs_last']);
             }
-            $datasets['sets'][$key]['json'] = json_encode($datasets['sets'][$key]['data']);
+
+            array_walk($tempdata, array('self', '_maybe_string_to_number')); // Get number values without format
+
+            $datasets[$letter]['name'] = $column; // First row is the name of column
+            $datasets[$letter]['data'] = $tempdata;
         }
 
-        $ckey = str_ireplace('-', '_', $render_options['html_id']);
+        // Declare compiling var
+        $chartjs = [];
+
+        // label
+        if (!in_array(strtolower($render_options['chartjs_label']), array_keys($datasets))) {
+            // TODO manage error
+        }
+        $chartjs['label'] = json_encode($datasets[strtolower($render_options['chartjs_label'])]['data']);
+
+        // Colors
+        $vcolors = explode(',', $render_options['chartjs_color']);
+        $acolors = [];
+        foreach ($vcolors as $key => $value) {
+            $acolors[] = self::$colors[$value];
+        }
+
+        // Data Sets
+        $vdata = explode(',', $render_options['chartjs_data']);
+
+        // Repeat colors to fill data
+        do {
+            $acolors = array_merge($acolors, $acolors);
+        } while (count($acolors) <= count($vdata));
+
+
+        // Data organi
+        foreach ($vdata as $key => $value) {
+            $letter = strtolower($value);
+            if (in_array($letter, array_keys($datasets))) {
+                $chartjs['sets'][] = array(
+                    'title' => $datasets[$letter]['name'],
+                    'color' => $acolors[$key],
+                    'json' => json_encode(array_values($datasets[$letter]['data']))
+                );
+            }
+        }
 
 
         // DataSets
-        foreach ($datasets['sets'] as $dkey => $dvalue) {
-            $datasets['ds'][] = "{
-                label: '{$dvalue['name']}',
+        foreach ($chartjs['sets'] as $dkey => $dvalue) {
+            $chartjs['ds'][] = "{
+                label: '{$dvalue['title']}',
                 fill: false,
-                backgroundColor: '{$colors[$dkey]}',
-                borderColor: '{$colors[$dkey]}',
+                backgroundColor: '{$dvalue['color']}',
+                borderColor: '{$dvalue['color']}',
                 pointRadius: .1,
                 pointHoverRadius: 5,
-                data: {$datasets['sets'][$dkey]['json']},
+                data: {$dvalue['json']},
             }";
         }
-        $datasets['rend'] = implode(',', $datasets['ds']);
+        $chartjs['rend'] = implode(',', $chartjs['ds']);
 
 
+        $htmlkey = str_ireplace('-', '_', $render_options['html_id']);
 
+        // Canvas element size
+        $canvas_size = "";
+        if (!empty($render_options['chartjs_width'])) {
+            $canvas_size .= " width=\"{$render_options['chartjs_width']}\"";
+        }
+        if (!empty($render_options['chartjs_height'])) {
+            $canvas_size .= " height=\"{$render_options['chartjs_height']}\"";
+        }
 
-        //print_r($datasets['rend']); die();
-
-        $json_chart_options = [];
-
-        $chartjs_divtag = "<div style=\"width:100%;\"><canvas id=\"canv_{$ckey}\"></canvas></div>\n";
+        $chartjs_divtag = "<div style=\"width:100%;\"><canvas id=\"canvas_{$htmlkey}\"$canvas_size></canvas></div>\n";
 
         $chartjs_script = <<<JS
 <script type="text/javascript">
     jQuery(document).ready(function(){
-        var config_{$ckey} = {
+        var config_{$htmlkey} = {
 			type: '{$chart}',
 			data: {
-				labels: {$datasets['label']['json']},
-				datasets: [{$datasets['rend']}]
+				labels: {$chartjs['label']},
+				datasets: [{$chartjs['rend']}]
 			},
 			options: {
 				responsive: true,
 				title: {
+                    text: '{$table['name']}',
 					display: false,
-					text: 'Title'
 				},
 				tooltips: {
 					mode: 'index',
 					intersect: false,
-
 				},
 				hover: {
 					mode: 'nearest',
@@ -281,32 +289,26 @@ class TablePress_Chartjs
 				scales: {
 					xAxes: [{
 						display: true,
-						scaleLabel: {
-							display: false,
-							labelString: ''
-						},
+						scaleLabel: { display: false, labelString: '', },
                         ticks: {
                             callback: function(value, index, values) {
                                 if (isNaN(value)) {
                                     return value;
                                 } else {
-                                    return Intl.NumberFormat().format((value));
+                                    return Intl.NumberFormat("es-MX").format((value));
                                 }
                             }
                         }
 					}],
 					yAxes: [{
 						display: true,
-						scaleLabel: {
-							display: false,
-							labelString: ''
-						},
+						scaleLabel: { display: false, labelString: '', },
                         ticks: {
                             callback: function(value, index, values) {
                                 if (isNaN(value)) {
                                     return value;
                                 } else {
-                                    return Intl.NumberFormat().format((value));
+                                    return Intl.NumberFormat("es-MX").format((value));
                                 }
                             }
                         }
@@ -315,42 +317,33 @@ class TablePress_Chartjs
 			}
 		};
 
-
-
-        var cjs_{$ckey} = document.getElementById('canv_{$ckey}').getContext('2d');
-        window.myLine = new Chart(cjs_{$ckey}, config_{$ckey});
-
-
+        var cjs_{$htmlkey} = document.getElementById('canvas_{$htmlkey}').getContext('2d');
+        window.myLine = new Chart(cjs_{$htmlkey}, config_{$htmlkey});
     });
 </script>
 JS;
 
-        return $chartjs_divtag.$chartjs_script;
+        return $chartjs_divtag . $chartjs_script;
     }
+
 
     /**
      * Convert a string to int or float, if it's a numeric string.
      *
-     * @since 0.6
-     *
      * @param string $string String that shall be converted to a number.
-     *
      * @return mixed Possibly converted string.
      */
-    protected static function _maybe_string_to_number($string)
+    protected static function _maybe_string_to_number(&$string)
     {
-        if (empty($string)) {
-            return 0;
-        }
-
-        if (!is_numeric($string)) {
-            return $string;
-        }
-
-        if ($string == (int) $string) { // Don't do explicit === check here!
-            return (int) $string;
-        } else {
-            return (float) $string;
+        if(preg_match('/^[0-9.,$ ]+$/', $string)) {
+            $string = filter_var($string, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+            if ($string == (int) $string) {
+                $string = (int) $string;
+            } else {
+                return (float) $string;
+            }
+        } elseif (empty($string)) {
+            $string = 0;
         }
     }
 } // class TablePress_Chartjs
